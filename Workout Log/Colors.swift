0dfb86dc28cbf0f54c3fb4d92e4af2a1d08c2a27
Milo Wyner <milowyner@gmail.com>
2020-04-19 15:57:51 -0700

    Add TextAndIconButton view filled with a new color added to assets and a Colors.swift file extending Color

---
+++ Workout Log/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  Workout Log
+//
+//  Created by Milo Wyner on 4/19/20.
+//  Copyright © 2020 Milo Wyner. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static var buttonBackground: Color { Color("Button Background") }
+}
---
+++ Workout Log/TextAndIconButton.swift
@@ -0,0 +1,23 @@
+//
+//  TextAndIconButton.swift
+//  Workout Log
+//
+//  Created by Milo Wyner on 4/19/20.
+//  Copyright © 2020 Milo Wyner. All rights reserved.
+//
+
+import SwiftUI
+
+struct TextAndIconButton: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 16)
+            .fill(Color.buttonBackground)
+    }
+}
+
+struct TextAndIconButton_Previews: PreviewProvider {
+    static var previews: some View {
+        TextAndIconButton()
+            .frame(width: 200, height: 50, alignment: .center)
+    }
+}
