9f366bce2341131554a51c55ceaae05487f9d09b
Milo Wyner <milowyner@gmail.com>
2020-04-19 14:19:17 -0700

    Initial commit

---
+++ Workout Log/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Workout Log
+//
+//  Created by Milo Wyner on 4/19/20.
+//  Copyright © 2020 Milo Wyner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Workout LogTests/Workout_LogTests.swift
@@ -0,0 +1,34 @@
+//
+//  Workout_LogTests.swift
+//  Workout LogTests
+//
+//  Created by Milo Wyner on 4/19/20.
+//  Copyright © 2020 Milo Wyner. All rights reserved.
+//
+
+import XCTest
+@testable import Workout_Log
+
+class Workout_LogTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
