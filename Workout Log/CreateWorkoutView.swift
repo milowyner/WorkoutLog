7de2e6e692b59718951c737be4bb914daf9f2048
Milo Wyner <milowyner@gmail.com>
2020-04-22 16:00:25 -0700

    Add CreateWorkoutView which allows a title to be entered, and has two CustomButtons at the bottom for saving the workout and adding a new exercise, respectively

---
+++ Workout Log/CreateWorkoutView.swift
@@ -0,0 +1,42 @@
+//
+//  CreateWorkoutView.swift
+//  Workout Log
+//
+//  Created by Milo Wyner on 4/22/20.
+//  Copyright © 2020 Milo Wyner. All rights reserved.
+//
+
+import SwiftUI
+
+struct CreateWorkoutView: View {
+    @State var workoutTitle: String = ""
+    
+    var body: some View {
+        VStack {
+            HStack {
+                TextField("Enter Title...", text: $workoutTitle)
+                    .font(.system(
+                        size: UIFont.preferredFont(forTextStyle: .largeTitle).pointSize,
+                        weight: .semibold))
+                Spacer()
+            }
+            Spacer()
+            HStack {
+                CustomButton(action: {
+                    print("Done button tapped")
+                }, icon: "icon-checkmark")
+                Spacer()
+                CustomButton(action: {
+                    print("Add exercise button tapped")
+                }, text: "Add Exercise", icon: "icon-plus")
+            }
+        }
+        .padding(32.0)
+    }
+}
+
+struct CreateWorkoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateWorkoutView()
+    }
+}
